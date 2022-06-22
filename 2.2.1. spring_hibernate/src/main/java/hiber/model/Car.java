ca4d734f6f025259c62652d46887d29973f03133
Georgiy Goloborodov <barstyle@mail.ru>
2022-06-22 16:42:12 +0300

    add class Car edit db.properties create CarDao

---
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
@@ -0,0 +1,39 @@
+package hiber.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+
+    @Id
+    private int series;
+
+    @Column(name = "model")
+    private String model;
+
+    public Car() {}
+
+    public Car(String model) {
+        setModel(model);
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+}
